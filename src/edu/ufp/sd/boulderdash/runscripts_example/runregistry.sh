59c7855bf2f41210db11cca3c56d0ebdcb1e50b3
Tiago Cardoso <tiagocardosoweb@gmail.com>
2017-05-20 17:56:31 +0100

    - Ignored runsscripts

---
+++ src/edu/ufp/sd/boulderdash/runscripts_example/runregistry.sh
@@ -0,0 +1,11 @@
+#REM ************************************************************************************
+#REM Description: start naming service (rmi registry)
+#REM Author: Rui S. Moreira
+#REM Date: 20/02/2014
+#REM ************************************************************************************
+source ./setenv.sh
+cd ${ABSPATH2CLASSES}
+#clear
+#pwd
+#rmiregistry &
+rmiregistry
